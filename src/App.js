97d7a9926ff25bc81ad76316100154bca5e9154c
Sachin Tom <sachintomacc@gmail.com>
2023-10-29 16:57:42 +0530

    render a sample geoplot

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import * as Plot from "@observablehq/plot";
-import PlotFigure from "./PlotFigure.js";
-import "./styles.css";
-import penguins from "./penguins.json";
-import timeseries from "./timeseries.json";
+import PlotGeo from "./PlotGeo.js"
+import "./styles.css"
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Plot + React</h1>
-      <h2>Penguins scatterplot</h2>
-      <PlotFigure
-        options={{
-          marks: [
-            Plot.dot(penguins, { x: "culmen_length_mm", y: "culmen_depth_mm" })
-          ]
-        }}
-      />
-      <h2>Time-series bar chart</h2>
-      <PlotFigure
-        options={{
-          x: { tickFormat: "d", interval: 1 },
-          marks: [Plot.barY(timeseries, { x: "year", y: "population" })]
-        }}
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <PlotGeo />
+        </div>
+    )
 }
